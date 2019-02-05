8688dab841b3afae9c5b33da965da9065282c6e6
Alejandro Quisbert <alexandro@autistici.org>
2019-02-05 16:22:43 -0400

    Fixing bad indentation

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ import os
 
 from telephone_operator import Operator
 
-import os
-
 map1 = {
-  '1': 0.9,
-  '268': 5.1,
-  '46': 0.17,
-  '4620': 0.0,
-  '468': 0.15,
-  '4631': 0.15,
-  '4673': 0.9,
-  '467320': 1.1
+    '1': 0.9,
+    '268': 5.1,
+    '46': 0.17,
+    '4620': 0.0,
+    '468': 0.15,
+    '4631': 0.15,
+    '4673': 0.9,
+    '467320': 1.1
 }
 
 map2 = {
-  '1': 0.92,
-  '44': 0.5,
-  '46': 0.2,
-  '467': 1.0,
-  '48': 1.2
+    '1': 0.92,
+    '44': 0.5,
+    '46': 0.2,
+    '467': 1.0,
+    '48': 1.2
 }
 
 operators = Operator()
@@ -43,52 +41,55 @@ header = "\
 
  
 colors = {
-  'blue': '\033[94m',
-  'pink': '\033[95m',
-  'green': '\033[92m',
+    'blue': '\033[94m',
+    'pink': '\033[95m',
+    'green': '\033[92m',
 }
- 
-def colorize(string, color):
-  if not color in colors: return string
-  return colors[color] + string + '\033[0m'
- 
-def show_operators():
-  print("List of avaliable operators")
-  operators.show_operators()
-  input("Press [Enter] to continue...")
- 
-def search():
-  print("Search lowest cost by number: ")
-  phone = input("Please, write a phone number: ")
-  low_cost = operators.low_cost(phone)
-  if low_cost == -1:
-    print('Prefix number does not exist registered')
-  else:
-    print('The lowest cost is {}'.format(low_cost))
 
-  input("Press [Enter] to continue...")
+
+def colorize(string, color):
+    if not color in colors: return string
+    return colors[color] + string + '\033[0m'
+
+
+def show_operators():
+    print("List of avaliable operators")
+    operators.show_operators()
+    input("Press [Enter] to continue...")
+
+
+def search():
+    print("Search lowest cost by number: ")
+    phone = input("Please, write a phone number: ")
+    low_cost = operators.low_cost(phone)
+    if low_cost == -1:
+        print('Prefix number does not exist registered')
+    else:
+        print('The lowest cost is {}'.format(low_cost))
+    input("Press [Enter] to continue...")
  
+
 menuItems = { 
-  "1": [show_operators, "Show operators"],
-  "2": [search, "Search telephone number"],
-  "3": [exit, "Exit"]
+    "1": [show_operators, "Show operators"],
+    "2": [search, "Search telephone number"],
+    "3": [exit, "Exit"]
 }
  
+
 def main():
-  while True:
-    os.system('clear')
-    print(colorize(header, 'pink'))
-    print(colorize('version 0.1\n', 'green'))
-    for item in menuItems:
-      print(colorize(item, 'blue'), menuItems[item][1])
-    choice = input(">> ")
-    try:
-      if int(choice) < 0 : raise ValueError
-      # Call the matching function
-      menuItems[choice][0]()
-    except (ValueError, IndexError):
-      pass
+    while True:
+        os.system('clear')
+        print(colorize(header, 'pink'))
+        print(colorize('version 0.1\n', 'green'))
+        for item in menuItems:
+            print(colorize(item, 'blue'), menuItems[item][1])
+        choice = input(">> ")
+        try:
+            if int(choice) < 0 : raise ValueError
+            # Call the matching function
+            menuItems[choice][0]()
+        except (ValueError, IndexError):
+            pass
  
 if __name__ == "__main__":
-  main()
-
+    main()
--- telephone_operator.py
+++ telephone_operator.py
@@ -1,56 +1,56 @@
 
 class Operator():
-  def __init__(self):
-    self.operators = []
+    def __init__(self):
+        self.operators = []
 
-  def add_operator(self, operator):
-    self.operators.append(operator)
+    def add_operator(self, operator):
+        self.operators.append(operator)
 
-  def fill_operator(self, operator):
-    prefixes_count = read('Number of prefixes: ')
-    operator = {}
-    for number in prefixes_count:
-      prefix = str(read('Insert prefix: '))
-      cost = read('Insert cost: ')
-      operator[prefix: cost]
-    operators.append(operator)
+    def fill_operator(self, operator):
+        prefixes_count = read('Number of prefixes: ')
+        operator = {}
+        for number in prefixes_count:
+            prefix = str(read('Insert prefix: '))
+            cost = read('Insert cost: ')
+            operator[prefix: cost]
+        operators.append(operator)
 
-  def show_operators(self):
-    for operator in self.operators:
-      print('\n PREFIX \t \t COST') 
-      print('=============================')
-      for prefix in operator:
-        print('{} \t \t {}'.format(prefix, operator[prefix]))
+    def show_operators(self):
+        for operator in self.operators:
+            print('\n PREFIX \t \t COST') 
+            print('=============================')
+            for prefix in operator:
+                print('{} \t \t {}'.format(prefix, operator[prefix]))
 
-  def search(self, map, phone, cost, large):
-    if len(phone) >=1 or len(map) > 0 and cost > -1:
-      newmap = {}
-      for x in map:
-        if phone[0] == x[0]:
-          if len(x) > 1:
-            newmap[x[1:]] = map[x]
-          else:
-            cost = map[x]
-      if len(newmap) == 1:
-        key, value = newmap.popitem()
-        if len(key) > 1:
-          newmap = {str(key): float(value)}
-          return self.search(newmap, phone[1:], cost, large)
+    def search(self, map, phone, cost, large):
+        if len(phone) >=1 or len(map) > 0 and cost > -1:
+            newmap = {}
+            for x in map:
+                if phone[0] == x[0]:
+                    if len(x) > 1:
+                        newmap[x[1:]] = map[x]
+                    else:
+                        cost = map[x]
+            if len(newmap) == 1:
+                key, value = newmap.popitem()
+                if len(key) > 1:
+                    newmap = {str(key): float(value)}
+                    return self.search(newmap, phone[1:], cost, large)
+                else:
+                    return value
+            else:
+                return self.search(newmap, phone[1:], cost, large)
         else:
-          return value
-      else:
-        return self.search(newmap, phone[1:], cost, large)
-    else:
-      return cost
+            return cost
 
 
-  def low_cost(self, phone):
-    costs = []
-    for operator in self.operators:
-      resul = self.search(operator, phone, -1, len(phone))
-      if resul != -1:
-        costs.append(resul)
-    if costs == []:
-      return -1
-    else:
-      return min(costs)
+    def low_cost(self, phone):
+        costs = []
+        for operator in self.operators:
+            resul = self.search(operator, phone, -1, len(phone))
+            if resul != -1:
+                costs.append(resul)
+        if costs == []:
+            return -1
+        else:
+            return min(costs)
